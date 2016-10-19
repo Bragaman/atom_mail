7c09d79b83d2141072ab85b12249604fb1f26dfa
Dmitry Bragin <cwbraga70@live.com>
2016-10-19 18:14:52 +0300

    add 3 homework

--- myfinance/finance/views.py
+++ myfinance/finance/views.py
@@ -6,15 +6,29 @@ from django.http import HttpResponse
 def home_page(request):
     return HttpResponse(
         '''
-        <h1>Hello, Dad!</h1>
-        <a href="/charges/"/>go to Dads page<a/>
+        <h1>Hello, People!</h1>
+        <a href="/charges/"/>go to your charges page<a/>
         '''
     )
 
 def charges_page(request):
     return HttpResponse(
         '''
-        <a href="/"/>go home<a/>
-        
+        <a href="/"/>go to home page<a/>
+        <table border="1">
+            <caption>Charges table</caption>
+            <tr>
+                <th>date time</th>
+                <th>sum</th>
+            </tr>
+            <tr>
+                <td>2016-09-09</td>
+                <td>-300</td>
+            </tr>
+            <tr>
+                <td>2016-09-10</td>
+                <td>500</td>
+            </tr>
+        </table>
         '''
     )
