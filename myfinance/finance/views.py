995c924fbb8feef1bef22732fd5506f392b08d51
Dmitry Bragin <cwbraga70@live.com>
2016-11-03 10:23:41 +0300

    add generator to charges view

--- myfinance/finance/views.py
+++ myfinance/finance/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.views.decorators.http import require_POST
-
+from datetime import date
+from decimal import Decimal
+from random import randint
 from finance.forms import *
 
 
@@ -12,7 +13,22 @@ def home_page(request):
 
 
 def charges_page(request):
-    return render(request, 'charges_page.html')
+    def random_transactions():
+        today = date.today()
+        start_date = today.replace(month=1, day=1).toordinal()
+        end_date = today.toordinal()
+        while True:
+            start_date = randint(start_date, end_date)
+            random_date = date.fromordinal(start_date)
+            if random_date >= today:
+                break
+            random_value = randint(-10000, 10000), randint(0, 99)
+            random_value = Decimal('%d.%d' % random_value)
+            yield random_date, random_value
+
+    gen = random_transactions()
+    charges = [c for c in gen]
+    return render(request, 'charges_page.html', {'charges': charges})
 
 
 # @require_POST
